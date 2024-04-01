180d999ffb25b6fd20f2a28153bb91de37b12c4b
brenward <bren.ward@gmail.com>
2024-04-01 13:09:05 +0100

    #Setting up a canDeactivateGuard

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,23 +1,38 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CanComponentDeactivate } from './can-deactivate.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
-  constructor(private serversService: ServersService,private route: ActivatedRoute) { }
+  constructor(private serversService: ServersService,private route: ActivatedRoute, private router:Router) { }
+  
+  canDeactivate(): boolean | Promise<boolean> | Observable<boolean>{
+    if(!this.allowEdit){
+      return true;
+    }
+
+    if(this.serverName !== this.server.name || this.serverStatus !== this.server.status && !this.changesSaved){
+      return confirm('Do you want to exit without saving?')
+    }else{
+      return true;
+    }
+  }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(+this.route.snapshot.params['id']);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
     this.route.queryParams.subscribe(
@@ -29,6 +44,8 @@ export class EditServerComponent implements OnInit {
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'],{relativeTo:this.route});
   }
 
 }
